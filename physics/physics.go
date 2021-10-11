2cd292293c777ff2e3b46f185b5f600cb2a95eff
Alīna Leladze <aleladze@edu.riga.lv>
2021-10-11 10:06:45 +0300

    adding physics

---
+++ physics/physics.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("The program finds the capacity of three inductors connected in parallel.")
+	fmt.Println("Enter inductance of three inductors: L1 L2 L3")
+	var L1, L2, L3 float64
+	fmt.Scanlnn(&L1, &L2, &L3)
+	
+
+
