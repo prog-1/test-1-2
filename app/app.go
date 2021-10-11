a44a93bac93e2d9aa8a87b77d10c4f33f7f4cf3d
Richard <richardsasalevic@gmail.com>
2021-10-11 09:46:48 +0300

    added comments in app

--- app/app.go
+++ app/app.go
@@ -5,15 +5,15 @@ import "fmt"
 // Replace the comments below with your explanations. Try to provide a
 // "high-level" idea, instead of writing literally what each line does.
 //
-// TODO: What does the program do?
+// TODO: This function takes 3-digit number and sorted in descending order
 func main() {
 	var num uint
-	fmt.Scan(&num)
-
-	// TODO: What do the variables a, b and c store?
+	//fmt.Scan(&num)
+	num = 751
+	// TODO: this variables splits number in 3 parts
 	a, b, c := num%10, num/10%10, num/100%10
 
-	// TODO: What do the three `if ... { ... }` statements below do?
+	// Sorts varibles a b c, so a is the largest and c is the lowest
 	if a < b {
 		a, b = b, a
 	}
@@ -24,7 +24,7 @@ func main() {
 		b, c = c, b
 	}
 
-	// TODO: What do the three lines below do?
+	// TODO: combines a b c into a number
 	num = a
 	num = num*10 + b
 	num = num*10 + c
